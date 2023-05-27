e85c639669ad10f86cb72ac6b153aa956c8749e0
Chris <2013206+kress95@users.noreply.github.com>
2023-05-27 18:54:26 -0300

    add nix shell and direnv

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  unstable = import <nixpkgs-unstable> {};
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    unstable.deno
+    elmPackages.elm
+    elmPackages.elm-format
+    elmPackages.elm-test-rs
+  ];
+}
